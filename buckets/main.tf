3b9b8280c58280c4bea6b643eece0d1a83b5906e
Rajasekhar Reddy <rajasekhar2178@gmail.com>
2023-03-22 13:02:33 +0530

    aws

--- buckets/main.tf
+++ buckets/main.tf
@@ -1,7 +1,12 @@
-resource "aws_s3_bucket" {
-  bucket = "raja-bucket"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "raja"
 
   tags = {
     Name        = "raja"
   }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
 }
